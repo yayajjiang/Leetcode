833737a68c287fcd278234e9123043f85c3fc55c
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-06-25 00:08:04 -0700

    Update 1491Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java

--- 1491Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java
+++ 1491Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.java
@@ -16,3 +16,8 @@ class Solution {
         
     }
 }
+
+//python
+// class Solution:
+//     def average(self, salary: List[int]) -> float:
+//         return ((sum(salary)-max(salary)-min(salary))/(len(salary)-2))

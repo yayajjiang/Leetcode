33631298aabd878ae2c3736ace3f50271a66b3cd
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-06-27 00:01:00 -0700

    Update and rename offer04Find_In_Partially_Sorted_Matrix.java to Offer04Find_In_Partially_Sorted_Matrix.java

--- Offer04Find_In_Partially_Sorted_Matrix.java
+++ Offer04Find_In_Partially_Sorted_Matrix.java
@@ -111,3 +111,25 @@ class Solution {
      }
           
 }
+
+
+//simplified
+class Solution {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int row = 0, col = n - 1;
+        while(row < m && col >= 0) {
+            if(matrix[row][col] > target) {//start from upper right corner
+                col--;
+            }else if(matrix[row][col] < target) {
+                row++;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
+}

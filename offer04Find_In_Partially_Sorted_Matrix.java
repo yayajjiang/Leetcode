7b2c5772e1b3966d47d2c1855877945db3a616ae
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-06-26 23:47:19 -0700

    medium, 06/26/2022, matrix

---
+++ offer04Find_In_Partially_Sorted_Matrix.java
@@ -0,0 +1,113 @@
+// class Solution {
+//     public boolean findNumberIn2DArray(int[][] matrix, int target) {
+//         if(matrix.length == 0){
+//             return false;
+//         }
+
+//         int row = matrix.length;
+//         int col = matrix[0].length;
+//         int arow=0; int acol=0;
+//         if(row==1){
+//             for(int j = 0; j<col; j++){
+//                 if(target == matrix[0][j]){
+//                     return true;
+//                 }
+//             }
+//             return false;
+//         }
+//         if(col==1){
+//             for(int i = 0; i<row; i++){
+//                 if(target == matrix[i][0]){
+//                     return true;
+//                 }
+//             }
+//             return false;
+                
+//         }
+//         for(int i = 0; i<row; i++){
+//             if(target <= matrix[i][0]){
+//                 arow = i+1;
+//             }
+//         }
+//         //System.out.println(row);
+//         for(int j = 0; j<col; j++){
+//             if(target <= matrix[0][j]){
+//                 acol = j+1;
+//             }
+            
+//         }
+//         //System.out.println(col);
+//         for(int i = 0; i<row; i++){
+//             for(int j=acol; j<col; j++){
+//                 if(target == matrix[i][j]){
+//                             //System.out.println(target + " "+matrix[i][j]);
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+class Solution {
+     public boolean findNumberIn2DArray(int[][] matrix, int target) {
+         if(matrix.length == 0){
+            return false;
+        }
+    
+        int row = matrix.length; int arow=0;
+        int col = matrix[0].length;
+        if(row==1){
+            for(int j = 0; j<col; j++){
+                if(target == matrix[0][j]){
+                    return true;
+                }
+            }
+            return false;
+        }
+        if(col==1){
+            for(int i = 0; i<row; i++){
+                if(target == matrix[i][0]){
+                    return true;
+                }
+            }
+            return false;
+                
+        }
+        col--;
+        while(col>=0){
+            if(target < matrix[0][col]){
+                col--;
+            }else{
+                break;
+            }
+        }
+        if(col<0){
+            return false;
+        }
+        while(arow<row){
+            if(target > matrix[arow][col]){
+                arow++;
+            }else{
+                break;
+            }
+        }
+        if(arow == row - 1 && target > matrix[arow][col]){
+            return false;
+        }
+        int i; int j;
+        //System.out.println(arow + " "+col);
+        for(i = arow; i < row; i++ ){
+            for (j = 0; j <= col; j++){
+                //System.out.println(i + " "+j);
+                 if(target == matrix[i][j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+     
+     }
+          
+}

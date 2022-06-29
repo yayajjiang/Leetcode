5e2b781e21e7684ef4007b6130ea04e515b31d6d
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-06-28 23:53:58 -0700

    2022/06/28, easy, sorted array

---
+++ 88Merge_Sorted_Array.java
@@ -0,0 +1,10 @@
+//copy, sort
+//nlogn
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+            System.arraycopy(nums2,0,nums1,m,n);
+            Arrays.sort(nums1);
+    }
+}
+

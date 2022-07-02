fe1e04e6bccf4109356724251c324e715b0ef9c8
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-07-02 01:08:56 -0700

    Update offer06Print_list_from_tail_to_head.java

--- offer06Print_list_from_tail_to_head.java
+++ offer06Print_list_from_tail_to_head.java
@@ -1,1 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        int num=0;
+        ListNode temp = head;
+        while(temp!= null){
+            //a[i] = head.val;
+            temp = temp.next;
+            //i++;
+            num++;
+        }
+        System.out.println(num);
+        int[] a = new int[num];
+        int i = 0;
+        while(head!= null){
+            a[num-i-1] = head.val;
+            head = head.next;
+            i++;
+        }
+        return a;
 
+    }
+}

7137e4c1e80b7c25bbf9401f11f33f737d77cc97
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-07-11 23:16:20 -0700

    Update and rename 141_Linked_List_Cycle.java to 141Linked_List_Cycle.java

---
+++ 141Linked_List_Cycle.java
@@ -0,0 +1,45 @@
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head == null || head.next ==null){
+            return false;
+        }
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)    return true;
+        }
+
+        return false;
+    }
+}
+
+//hashset
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {  //return true if not in the set, else return false
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
+
+author：LeetCode-Solution
+link：https://leetcode.cn/problems/linked-list-cycle/solution/huan-xing-lian-biao-by-leetcode-solution/

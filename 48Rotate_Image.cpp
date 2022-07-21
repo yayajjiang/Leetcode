2387f86a65c2056d410c6041d1e6d9b7228f39ab
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-07-21 00:14:41 -0700

    Update 48Rotate_Image.cpp

--- 48Rotate_Image.cpp
+++ 48Rotate_Image.cpp
@@ -1,1 +1,27 @@
 
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> res = matrix;
+        vector<int> arr;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr.push_back(matrix[i][j]);
+            }
+        }
+        vector<int> arr1 = arr;
+        for (int i = 0; i < arr.size(); i++) {
+            arr1[(i%m) * m + (m-1-i/m) ] = arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = arr1[i * m + j];
+            }
+        }
+        
+    
+    }
+};

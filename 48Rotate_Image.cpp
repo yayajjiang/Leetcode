6529fcfc64068a1849678c74ab4e372f8cb2b156
Jinya Jiang <60903767+yayajjiang@users.noreply.github.com>
2022-07-21 00:49:08 -0700

    2d array, add another solution of flipping

--- 48Rotate_Image.cpp
+++ 48Rotate_Image.cpp
@@ -25,3 +25,27 @@ public:
     
     }
 };
+
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // level flipping
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                swap(matrix[i][j], matrix[n - i - 1][j]);
+            }
+        }
+        // diagonal flipping
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
+
+author：LeetCode-Solution
+link：https://leetcode.cn/problems/rotate-image/solution/xuan-zhuan-tu-xiang-by-leetcode-solution-vu3m/
